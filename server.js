0c40f5723c4295662dcbd7284d4cdbb12d4357d1
Dong Zhao <dzhao@redhat.com>
2017-01-24 16:57:26 +0800

    added authen source

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ if (mongoURL == null) {
     }
     // Provide UI label that excludes user id and pw
     mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase+"?authSource=admin";
     console.log("mongoURLLabel: " + mongoURLLabel);
     console.log("mongoURL: " + mongoURL);
 
